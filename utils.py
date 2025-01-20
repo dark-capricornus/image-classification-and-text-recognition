7f77e0c3dea7845d0a1025ef509462cfc0c9daa7
Harish <harishdeepabalavenkatraman@gmail.com>
2025-01-20 15:34:39 +0530

    cleaning repo

--- utils.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def preprocess_image(image_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-    return thresh
